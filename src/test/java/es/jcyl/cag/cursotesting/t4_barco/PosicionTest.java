580cb3a28541c5efa1f2d9f97fbfbe6d9f346935
Alberto Fernandez <infjaf@gmail.com>
2020-11-06 11:44:11 +0100

    Test clase posicion

---
+++ src/test/java/es/jcyl/cag/cursotesting/t4_barco/PosicionTest.java
@@ -0,0 +1,30 @@
+package es.jcyl.cag.cursotesting.t4_barco;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class PosicionTest {
+	
+	public PosicionTest() {
+		super();
+	}
+	
+	@Test
+	public void mantieneCoorenadas() {
+		Posicion p = new Posicion (1,2);
+		Assert.assertEquals(1, p.getX());
+		Assert.assertEquals(2, p.getY());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void noAdmiteNegativosEnX() {
+		new Posicion(-1, 10);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void noAdmiteNegativosEnY() {
+		new Posicion(10, -1);
+	}
+
+}
