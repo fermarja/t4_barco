880836cf952c0c1c82e9643e8b465b4e929bf885
Alberto Fernandez <infjaf@gmail.com>
2020-11-06 11:47:37 +0100

    Barco con posicion y direccion inicial

--- src/main/java/es/jcyl/cag/cursotesting/t4_barco/Barco.java
+++ src/main/java/es/jcyl/cag/cursotesting/t4_barco/Barco.java
@@ -2,4 +2,20 @@ package es.jcyl.cag.cursotesting.t4_barco;
 
 public class Barco {
 
+	private Posicion posicion;
+	private Direccion direccion;
+	
+	public Barco(Posicion posicion, Direccion direccion) {
+		super();
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public Direccion getDireccion() {
+		return direccion;
+	}
+	
+	
 }
